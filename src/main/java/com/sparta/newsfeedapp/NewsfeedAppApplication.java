22fed01344c1bf621e0b7e5f61d66316706675b5
leejinuk <leejinouk123@kakao.com>
2024-06-04 11:04:33 +0900

    add: Project Init

---
+++ src/main/java/com/sparta/newsfeedapp/NewsfeedAppApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.newsfeedapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsfeedAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsfeedAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/newsfeedapp/NewsfeedAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.newsfeedapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsfeedAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
